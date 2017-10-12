ef2b74cebb75e66c9d736cbf6268ca1d3b67a4b7
Robert Main <lenswipe@gmail.com>
2017-10-11 21:42:27 -0400

    Promises
    Use promises and their resoltion or rejection to handle errors and/or
    success
    
    Fix #1

--- backend/src/media_providers/disk/DiskStrategy.ts
+++ backend/src/media_providers/disk/DiskStrategy.ts
@@ -1,5 +1,6 @@
 import * as ffmpeg from 'fluent-ffmpeg';
 import * as fs from "fs";
+import * as path from 'path';
 import { Readable } from 'stream';
 
 import SongFactory from '../SongFactory'
@@ -7,15 +8,21 @@ import Song from '../../Song'
 import MediaProvider from '../MediaProvider';
 
 class DiskSongFactory implements SongFactory{
+
+    private songs_directory : string;
+
+    constructor(songs_directory : string){
+        this.songs_directory = songs_directory;
+    }
     
-    getSong(file_path: string): Promise<Song> {
-        return new Promise((resolve, reject) => ffmpeg(file_path).ffprobe( (err, stream_metadata) => {
+    public getSong(file_path: string): Promise<Song> {
+        return new Promise((resolve, reject) => ffmpeg(this.songs_directory + path.sep + file_path).ffprobe( (err, stream_metadata) => {
             if (err) {
                 reject(err)
             } else {
                 let id3_data = stream_metadata.format.tags;
                 resolve(new Song(
-                    file_path,
+                    this.songs_directory + path.sep + file_path,
                     id3_data.title || '',
                     id3_data.artist || '',
                     id3_data.album || '',
@@ -26,17 +33,21 @@ class DiskSongFactory implements SongFactory{
             }
         }))
     }
-    
+
 }
 
-class DiskAdapter implements MediaProvider{
-    public getAudio(file_path : string): Readable {
-        if(fs.existsSync(file_path)) {
-            return fs.createReadStream(file_path);
-        } else {
-            throw new Error('Unable to locate file ' + file_path + ' on disk')
-        }
+class DiskAdapter implements MediaProvider {
+
+    public getAudio(file_path : string): Promise<Readable> {
+        return new Promise((resolve, reject) => {
+            if(fs.existsSync(file_path)) {
+                resolve(fs.createReadStream(file_path));
+            } else {
+                reject('unable to find file: ' + file_path  + ' on disk');
+            }
+        })
     }
+    
 }
 
 export default DiskSongFactory;
