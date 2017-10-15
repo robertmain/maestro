98f9971f83e5c2400dbfae9192af133daed936c8
Robert Main <lenswipe@gmail.com>
2017-10-14 23:57:24 -0400

    Song Optional Fields
    Provide tests for optional song fields

--- test/song.ts
+++ test/song.ts
@@ -34,8 +34,11 @@ describe('A song', () => {
         it('represents the length of the song in seconds', () => {
             expect(song).to.have.property('duration');
             expect(song.duration).to.equal(30);
-            expect(song.duration).to.be.a('number');
         })
+        
+        it('is an ordinal number', () => {
+            expect(song.duration).to.be.a('number');            
+        });
     });
 
     describe('stream adapter', () => {
@@ -44,6 +47,54 @@ describe('A song', () => {
             expect(song).to.have.property('stream_adapter');
             expect(song).to.respondTo('getAudio');
         })
+    });
+
+    describe ('title', () => {
+        it('represents the title field of song metadata', () => {
+            const song = new Song('song.mp3', 30, <MediaProvider>{}, 'Graceland');
+            expect(song.title).to.equal('Graceland');
+        });
+
+        it('provides a default value if omitted', () => {
+            const song = new Song('song.mp3', 30, <MediaProvider>{});
+            expect(song.title).to.equal('Title Unavailable');
+        });
+    });
+
+    describe ('artist', () => {
+        it('represents the artist field of song metadata', () => {
+            const song = new Song('song.mp3', 30, <MediaProvider>{}, 'Graceland', 'Paul Simon');
+            expect(song.artist).to.equal('Paul Simon');
+        });
+
+        it('provides a default value if omitted', () => {
+            const song = new Song('song.mp3', 30, <MediaProvider>{});
+            expect(song.artist).to.equal('Unknown Artist');
+        });
+    });
+
+    describe ('album', () => {
+        it('represents the album field of song metadata', () => {
+            const song = new Song('song.mp3', 30, <MediaProvider>{}, 'Graceland', 'Paul Simon', 'Graceland');
+            expect(song.album).to.equal('Graceland');
+        });
+
+        it('provides a default value if omitted', () => {
+            const song = new Song('song.mp3', 30, <MediaProvider>{});
+            expect(song.album).to.equal('Unknown Album');
+        });
+    });
+
+    describe ('genre', () => {
+        it('represents the genre field of song metadata', () => {
+            const song = new Song('song.mp3', 30, <MediaProvider>{}, 'Graceland', 'Paul Simon', 'Graceland', 'Pop/Worldbeat');
+            expect(song.genre).to.equal('Pop/Worldbeat');
+        });
+
+        it('provides a default value if omitted', () => {
+            const song = new Song('song.mp3', 30, <MediaProvider>{});
+            expect(song.genre).to.equal('Unknown Genre');
+        });
     });
 
 });
