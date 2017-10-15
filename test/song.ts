320fa6a46c3620ec0d5da0b04ee7d40dd104342a
Robert Main <lenswipe@gmail.com>
2017-10-14 20:04:42 -0400

    Mandatory Song Parameters
    Add tests for mandatory parameters on the song object

--- test/song.ts
+++ test/song.ts
@@ -18,16 +18,32 @@ var expect = chai.expect;
 /**
  * Unit tests
  */
-describe('A song:', () => {
-    const song = new Song('song.mp3', 30, <MediaProvider>{});
+describe('A song', () => {
     
     describe('identifier', () => {
-        it('is required', (done) => {
+        const song = new Song('song.mp3', 30, <MediaProvider>{});
+        it('represents the location of a single file', () => {
             expect(song).to.have.property('identifier');
             expect(song.identifier).to.equal('song.mp3')
             expect(song.identifier).to.be.a('string');
-            done();
         });
+    });
+
+    describe('duration', () => {
+        const song = new Song('song.mp3', 30, <MediaProvider>{});
+        it('represents the length of the song in seconds', () => {
+            expect(song).to.have.property('duration');
+            expect(song.duration).to.equal(30);
+            expect(song.duration).to.be.a('number');
+        })
+    });
+
+    describe('stream adapter', () => {
+        const song = new Song('song.mp3', 30, <MediaProvider>{});
+        it('provides a mechanism for audio retrieval', () => {
+            expect(song).to.have.property('stream_adapter');
+            expect(song).to.respondTo('getAudio');
+        })
     });
 
 });
