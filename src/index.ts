be604693bb36a68d50d9b2485593289a6d0a78bf
Robert Main <lenswipe@gmail.com>
2017-11-02 17:53:21 -0400

    Song Information API
    Currently got a basic song informtion API going on over HTTP. This uses
    the jayson server as express middleware

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import "reflect-metadata";
 import { Container } from 'inversify';
 import { interfaces as IExpressUtils, InversifyExpressServer, TYPE } from 'inversify-express-utils';
+import * as bodyParser from 'body-parser';
+import * as jayson from 'jayson';
 
 import Song from './api/controllers/Song';
 import { TYPES } from './Types';
@@ -12,15 +14,24 @@ import DiskFactory from './api/services/media_providers/disk/DiskFactory';
 
 let container = new Container();
 
-container.bind<IExpressUtils.Controller>(TYPE.Controller).to(Song).whenTargetNamed('Song');
+container.bind<IExpressUtils.Controller>(TYPE.Controller).to(Song);
 container.bind<AudioSource>(TYPES.AudioSource).to(DiskSource);
 container.bind<AudioFactory>(TYPES.AudioFactory).to(DiskFactory);
 container.bind<any>(TYPES.Config).toConstantValue(config);
 
-// create server
-let server = new InversifyExpressServer(container);
+let songController = container.get<Song>(TYPE.Controller);
 
-server
+let RPCServer = jayson.server({
+    getSong: (args: any, cb: Function) => songController.getSong(args, cb)
+});
+
+let ExpressServer = new InversifyExpressServer(container);
+
+ExpressServer
+    .setConfig((app) => {
+        app.use(bodyParser.json())
+        app.use(RPCServer.middleware())
+    })
     .build()
     .listen(config.webServer.port, config.webServer.bind_address, () => {
         console.log('Now listening on ' + config.webServer.bind_address + ':' + config.webServer.port);
