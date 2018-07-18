fb05b7e3d7fa3026620810bc0e5ee4bdfc384c36
Robert Main <lenswipe@gmail.com>
2018-07-18 00:08:02 -0400

    HMR Port Configuration
    Grab the configuration file and update HMR config to use the port in the configuration file

--- src/server/main.hmr.ts
+++ src/server/main.hmr.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from './config';
 
 declare const module: any;
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    await app.listen(config.http.port);
 
     if (module.hot) {
         module.hot.accept();
