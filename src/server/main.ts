d2c9bb22347c6546f2026a22b39fe9d1d746eb5f
Robert Main <lenswipe@gmail.com>
2021-10-11 23:37:43 -0400

    Refactor main entry point

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import { config } from './config';
 
-async function bootstrap() {
+const { PORT, NODE_ENV } = process.env;
+
+(async (): Promise<void> => {
     const app = await NestFactory.create(AppModule);
-    await app.listen(config.http.port);
-}
-bootstrap();
+
+    app.setGlobalPrefix('api');
+    if (NODE_ENV !== 'production') {
+        const options = new DocumentBuilder().build();
+
+        const document = SwaggerModule.createDocument(app, options);
+        SwaggerModule.setup('docs', app, document);
+    }
+    app.useGlobalPipes(new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+    }));
+    await app.listen(PORT);
+})();
