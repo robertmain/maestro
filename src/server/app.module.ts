933f5cd991aa313304766fc2e44aaf225ac9aec5
Robert Main <lenswipe@gmail.com>
2021-10-12 21:56:17 -0400

    Remove old boilerplate code

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { FfmpegModule } from './ffmpeg/Ffmpeg.module';
 
 @Module({
     imports: [
         FfmpegModule,
     ],
-    controllers: [AppController],
-    providers: [AppService],
+    controllers: [],
+    providers: [],
 })
 export class AppModule { }
