6fe0ea1d6352f1180f2e4f2598a5f0e1ab836238
Robert Main <lenswipe@gmail.com>
2021-10-13 00:30:34 -0400

    Config Module
    Move to official NestJS config module (instead of DIY). The official
    nest one supports a lot of out of the box stuff like reading a dotenv
    file, reading runtime env vars etc.

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { FfmpegModule } from './ffmpeg/ffmpeg.module';
 
 @Module({
     imports: [
+        ConfigModule.forRoot({
+            ignoreEnvFile: true,
+            cache: false,
+            isGlobal: true,
+        }),
         FfmpegModule,
     ],
     controllers: [],
