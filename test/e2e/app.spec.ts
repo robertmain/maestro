3d8c3bb74d52f34e0d8dbe717a39198458c4e71e
Robert Main <lenswipe@gmail.com>
2021-10-13 19:26:04 -0400

    Remove useless test
    This test was for a controller that no longer exists.

--- test/e2e/app.spec.ts
+++ test/e2e/app.spec.ts
@@ -1,22 +1,21 @@
 import request from 'supertest';
 import { Test } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpServer, INestApplication } from '@nestjs/common';
 import { AppModule } from '../../src/server/app.module';
 
 describe('AppController (e2e)', () => {
-    let app: INestApplication;
+    let api: HttpServer;
 
     beforeAll(async () => {
         const moduleFixture = await Test.createTestingModule({
             imports: [AppModule],
         }).compile();
 
-        app = moduleFixture.createNestApplication();
+        const app = moduleFixture.createNestApplication();
         await app.init();
+        api = app.getHttpServer();
     });
-
-    it('/GET /', () => request(app.getHttpServer())
-        .get('/')
-        .expect(200)
-        .expect('Hello World!'));
+    it.skip('', () => {
+        // Do something in here
+    });
 });
