8daae11cd35e2b1395d5db30576ff65c95c8f5e6
Robert Main <lenswipe@gmail.com>
2022-06-19 17:40:20 -0400

    indentation

--- src/server/media/types.ts
+++ src/server/media/types.ts
@@ -8,11 +8,11 @@
  * ```
  *
  * @param sampleRate  The sample rate of a song, this is usually safe to leave at 44100
- * @param title        The song title (song metadata - this is displayed to the user)
- * @param artist       The song artist (song metadata - this is displayed to the user)
- * @param album        The song album (song metadata - this is displayed to the user)
- * @param genre        The song genre(s) (song metadata - this is displayed to the user). Some songs do have more
- *                     than one genre, which is why this is an array of strings, rather than just a string.
+ * @param title       The song title (song metadata - this is displayed to the user)
+ * @param artist      The song artist (song metadata - this is displayed to the user)
+ * @param album       The song album (song metadata - this is displayed to the user)
+ * @param genre       The song genre(s) (song metadata - this is displayed to the user). Some songs do have more
+ *                    than one genre, which is why this is an array of strings, rather than just a string.
  */
 export interface SongMetaData {
     duration: number;
